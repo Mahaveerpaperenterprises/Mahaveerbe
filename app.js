ba3e2adb79799a58f735515fb5a58215a1322426
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-07-31 14:07:15 +0530

    handling both local and production deployment

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require('dotenv').config(); // Load env variables from .env for local development or vercel deployment
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+
+// Route imports
 const navlinksRouter = require('./routes/navlinks');
 const prodlinksRouter = require('./routes/products');
 const categoriesRouter = require('./routes/categories');
@@ -10,6 +14,7 @@ const ordersRouter = require('./routes/orders');
 
 const app = express();
 
+// Allow listed origins for local development
 const corsOptions = {
   origin: ['http://localhost:3000', 'http://192.168.0.106:3000'],
   credentials: true,
@@ -19,6 +24,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// Routes
 app.use('/api/navlinks', navlinksRouter);
 app.use('/api/products', prodlinksRouter);
 app.use('/api/categories', categoriesRouter);
@@ -26,10 +32,12 @@ app.use('/api/auth', authRouter);
 app.use('/api/upload', uploadRoute);
 app.use('/api/orders', ordersRouter);
 
+// Start server
+const PORT = process.env.PORT || 5000;
+
 if (require.main === module) {
-  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
-    console.log(`🟢 Server running locally → http://localhost:${PORT}`);
+    console.log(`🟢 Server running on → http://localhost:${PORT}`);
   });
 }
 
