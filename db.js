729fbb220f5aec81668b6bfe2a0ea497f15f0634
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-08-11 01:40:47 +0530

    deploy issue 3

--- db.js
+++ db.js
@@ -11,14 +11,9 @@ module.exports = pool;        // every file can now:  const pool = require('./db
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const dbUrl = process.env.DATABASE_URL ||
-  'postgresql://2p9c58:xau_P0AXHjI0JWau97sEpBWNuFry3gMSm2b00@eu-central-1.sql.xata.sh/test:main?sslmode=require';
-
-const isLocal = dbUrl.includes('localhost');
-
 const pool = new Pool({
-  connectionString: dbUrl,
-  ssl: isLocal ? false : { rejectUnauthorized: false }
+  connectionString : process.env.DATABASE_URL,
+  ssl              : { rejectUnauthorized: false }
 });
 
-module.exports = pool;
+module.exports = pool;       
